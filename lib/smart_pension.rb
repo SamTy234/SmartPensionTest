132441acb60d48f4a36433597ef8abb2d3d5572a
Samuel Gasper <1516828@my.brunel.ac.uk>
2019-07-18 16:07:19 +0100

    Completed both required functionalities

--- lib/smart_pension.rb
+++ lib/smart_pension.rb
@@ -4,35 +4,60 @@ class Database
 
   def initialize(file_path)
     @file_path = file_path
-    @pages_with_ip = []
-    @pages = []
   end
 
-  def handle_pages_array
+  def parse_file
+    pages_with_ip =[]
+
     CSV.foreach(@file_path) do |row|
-      @pages_with_ip << row[0].split(' ')
+      pages_with_ip << row[0].split(' ')
     end
 
-    @pages_with_ip.each do |page|
-        @pages << page[0]
-    end
-
-    @pages.sort!
-    return @pages
+    return pages_with_ip
   end
 
-  def page_count(pages)
+  def populate_pages_array(data)
+    pages = []
 
-    @pages.each_cons(2) do |current_page, next_page|
-      first_page
+    data.each do |page|
+      pages << page[0]
     end
 
+    pages.sort!
+    return pages
+  end
+
+  def populate_hash(data)
+    data_hash = Hash.new(){|hsh, key| hsh[key] =[]}
+
+    data.each{|key, value| data_hash[key] << value}
+
+    data_hash.each do |key, value|
+      value.uniq!
+      data_hash[key] = value.count
+    end
+
+    data_hash.sort_by
+
+    return data_hash.sort_by{ |key, value| -value}.to_h
+  end
+
+  def count_page_visits(pages)
+
+    page_count = Hash.new(0)
+
+    pages.each_with_object(page_count) do |page, new_hash|
+      new_hash[page] += 1
+    end
+
+    return page_count.sort_by{ |key, value| -value}.to_h
   end
 
 
 end
 
 d = Database.new('webserver.log')
-d.page_count(d.handle_pages_array)
+p d.populate_hash(d.parse_file)
+# p d.count_page_visits(d.handle_pages_array)
 
 
